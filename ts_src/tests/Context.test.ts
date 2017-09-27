57beaf473009e264a12e016251611d22faa01e83
David Li-Bland <david.libland@gmail.com>
2017-09-27 16:06:30 -0700

    Updated Formatting.

--- ts_src/tests/Context.test.ts
+++ ts_src/tests/Context.test.ts
@@ -4,10 +4,10 @@
  */
 
 import { initializeLTVWithContext } from "../localTextVisor/Context";
-import {LANGUAGE_ALGORITHM_TYPE, TOKENIZER_TYPE, REWARD_TYPE, QUALITY_TYPE} from "../localTextVisor/Enums";
+import { LANGUAGE_ALGORITHM_TYPE, TOKENIZER_TYPE, REWARD_TYPE, QUALITY_TYPE } from "../localTextVisor/Enums";
 import "ts-jest";
 
 test("Initialize LTV with Identity Predictor", () => {
-    const idPipeline = initializeLTVWithContext<string>({moduleType: LANGUAGE_ALGORITHM_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER}, {moduleType: REWARD_TYPE.LENGTH_DIFFERENCE});
+    const idPipeline = initializeLTVWithContext<string>({ moduleType: LANGUAGE_ALGORITHM_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER }, { moduleType: REWARD_TYPE.LENGTH_DIFFERENCE });
     expect(idPipeline.predict("abracadabra", 5, 0, QUALITY_TYPE.EXPECTED_REWARD).map(wPred => wPred.prediction)).toEqual(["abracadabra"]);
 });
