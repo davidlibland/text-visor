59cedfde9c4e631b6fa77a213f4295ec26f97f2e
David Li-Bland <david.libland@gmail.com>
2017-10-09 12:23:29 -0700

    Added tests for the standard LTV modules

--- ts_src/tests/Context.test.ts
+++ ts_src/tests/Context.test.ts
@@ -3,54 +3,79 @@
  * @desc Some basic tests for the context module.
  */
 
-import { initializeLTVWithContext } from "../localTextVisor/Context";
-import { LANGUAGE_MODULE_TYPE, TOKENIZER_TYPE, REWARD_MODULE_TYPE, QUALITY_MODULE_TYPE } from "../localTextVisor/Enums";
 import "ts-jest";
-import { Tree, sortedInsert } from "../localTextVisor/plaintext/Tree";
+import { initializeLTVWithContext, RewardModuleSpecsPSG, RewardModuleSpecsSLD } from "../localTextVisor/Context";
+import { LANGUAGE_MODULE_TYPE, QUALITY_MODULE_TYPE, REWARD_MODULE_TYPE, TOKENIZER_TYPE } from "../localTextVisor/Enums";
+import { sortedInsert, Tree } from "../localTextVisor/plaintext/Tree";
 
 test("Initialize LTV with Identity Predictor", () => {
-    const idPipeline = initializeLTVWithContext({ moduleType: LANGUAGE_MODULE_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER }, { moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE }, {trie:{node:"",children:[],data:[]}, prior:{}});
+    const idPipeline = initializeLTVWithContext({ moduleType: LANGUAGE_MODULE_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER }, { moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE }, {trie: {node: "", children: [], data: []}, prior: {}});
 
     const result = idPipeline.predict("abracadabra", 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD)
-        .map(wPred => wPred.prediction);
+        .map((wPred) => wPred.prediction);
     expect(result).toEqual(["abracadabra"]);
 });
 
-type tokenData = {
+interface TokenData {
     prediction: string;
+}
+
+const testTree: Tree<string, TokenData> = { node: "root", children: [], data: [] };
+sortedInsert(testTree, "heart attack".split(""), { prediction: "heart attack" });
+sortedInsert(testTree, "health risk".split(""), { prediction: "health risk" });
+sortedInsert(testTree, "hepatitis".split(""), { prediction: "hepatitis" });
+sortedInsert(testTree, "jaundice".split(""), { prediction: "jaundice" });
+sortedInsert(testTree, "heal".split(""), { prediction: "heal" });
+sortedInsert(testTree, "heat".split(""), { prediction: "heat" });
+const prior = {
+    "heal": 2,
+    "health risk": 1,
+    "heart attack": 2,
+    "heat": 0,
+    "hepatitis": 3,
+    "jaundice": 25,
 };
 
-test("Initialize LTV with Fuzzy Tree Search Predictor", () => {
-    const testTree: Tree<string, tokenData> = { node: "root", children: [], data: [] };
-    sortedInsert(testTree, "heart attack".split(""), { prediction: "heart attack" });
-    sortedInsert(testTree, "health risk".split(""), { prediction: "health risk" });
-    sortedInsert(testTree, "hepatitis".split(""), { prediction: "hepatitis" });
-    sortedInsert(testTree, "jaundice".split(""), { prediction: "jaundice" });
-    sortedInsert(testTree, "heal".split(""), { prediction: "heal" });
-    sortedInsert(testTree, "heat".split(""), { prediction: "heat" });
-    const prior = {
-        "heart attack": 2,
-        "health risk": 1,
-        "hepatitis": 3,
-        "heal": 2,
-        "heat": 0,
-        "jaundice": 25
-    };
+test("Initialize LTV with Fuzzy Tree Search Predictor and string length difference reward", () => {
     const languageSpecs = {
-        moduleType: LANGUAGE_MODULE_TYPE.FUZZY_TRIE_SEARCH,
         maxEditDistance: 1,
-        tokenizerType: TOKENIZER_TYPE.WORD
+        moduleType: LANGUAGE_MODULE_TYPE.FUZZY_TRIE_SEARCH,
+        tokenizerType: TOKENIZER_TYPE.WORD,
     };
-    const rewardSpecs = {
-        moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE
+    const rewardSpecs: RewardModuleSpecsSLD = {
+        moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE,
     };
-    const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior: prior });
+    const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior });
     const input = { input: "who should we hea", cursorPosition: 15 };
-    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD).map(wPred => wPred.prediction);
+    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD).map((wPred) => wPred.prediction);
     expect(results).toEqual([
         "who should we heart attack",
         "who should we hepatitis",
         "who should we health risk",
-        "who should we heal"
+        "who should we heal",
+    ]);
+});
+
+test("Initialize LTV with Fuzzy Tree Search Predictor and prob-not-reject reward", () => {
+    const languageSpecs = {
+        maxEditDistance: 1,
+        moduleType: LANGUAGE_MODULE_TYPE.FUZZY_TRIE_SEARCH,
+        tokenizerType: TOKENIZER_TYPE.WORD,
+    };
+    const rewardSpecs: RewardModuleSpecsPSG = {
+        moduleType: REWARD_MODULE_TYPE.PROB_OF_NOT_REJECTING_SYMBOLS_GAINED,
+        rejectionLogit: 0,
+    };
+    const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior });
+    const input = { input: "who should we hea", cursorPosition: 15 };
+    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD).map((wPred) => {
+        console.log(wPred);
+        return wPred.prediction;
+    });
+    expect(results).toEqual([
+        "who should we heart attack",
+        "who should we hepatitis",
+        "who should we heal",
+        "who should we health risk",
     ]);
 });
---
+++ ts_src/tests/StandardLTVModules.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @file StandardLTVModules.test.ts
+ * @desc Some basic tests for the StandardLTVModules.
+ */
+
+import "ts-jest";
+import {
+    ProbOfNotRejectingSymbolsGainedDifferential,
+} from "../localTextVisor/StandardLTVModules";
+
+test("Testing the probability of not rejecting symbols gained differential", () => {
+    const differential = new ProbOfNotRejectingSymbolsGainedDifferential(0);
+    expect(differential.evaluate("he", "heal")).toEqual(.75);
+});
+
+test("Testing the probability of not rejecting symbols gained differential", () => {
+    const differential = new ProbOfNotRejectingSymbolsGainedDifferential(Math.log(0.1 / (0.9)));
+    expect(differential.evaluate("bo", "bo")).toEqual(0);
+    expect(differential.evaluate("bo", "bob")).toEqual(1 - 0.1);
+    expect(differential.evaluate("bo", "bobb")).toEqual(1 - 0.01);
+    expect(differential.evaluate("bo", "bobby")).toEqual(1 - 0.001);
+    expect(differential.evaluate("bo", "bobby flay")).toEqual(1 - 0.00000001);
+});
