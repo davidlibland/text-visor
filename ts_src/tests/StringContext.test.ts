a3bff8c39ccfb41b7c3ead960c2811708b571f78
David Li-Bland <david.libland@gmail.com>
2017-10-18 14:04:15 -0700

    Add new test.

--- ts_src/tests/StringContext.test.ts
+++ ts_src/tests/StringContext.test.ts
@@ -12,7 +12,11 @@ import { LANGUAGE_MODULE_TYPE, QUALITY_MODULE_TYPE, REWARD_MODULE_TYPE, TOKENIZE
 import { sortedInsert, Tree } from "../localTextVisor/plaintext/Tree";
 
 test("Initialize LTV with Identity Predictor", () => {
-    const idPipeline = initializeLTVWithContext({ moduleType: LANGUAGE_MODULE_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER }, { moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE }, {trie: {node: "", children: [], data: []}, prior: {}});
+    const idPipeline = initializeLTVWithContext(
+        { moduleType: LANGUAGE_MODULE_TYPE.IDENTITY, tokenizerType: TOKENIZER_TYPE.CHARACTER },
+        { moduleType: REWARD_MODULE_TYPE.LENGTH_DIFFERENCE },
+        {trie: {node: "", children: [], data: []}, prior: {}},
+        );
 
     let result = idPipeline.predict("abracadabra", 5, 0, QUALITY_MODULE_TYPE.CONFIDENCE)
         .map((wPred) => wPred.prediction);
@@ -54,7 +58,8 @@ test("Initialize LTV with Fuzzy Tree Search Predictor and string length differen
     };
     const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior });
     const input = { input: "who should we hea", cursorPosition: 15 };
-    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD).map((wPred) => wPred.prediction);
+    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD)
+        .map((wPred) => wPred.prediction);
     expect(results).toEqual([
         "who should we heart attack",
         "who should we health risk",
@@ -75,10 +80,34 @@ test("Initialize LTV with Fuzzy Tree Search Predictor and prob-not-reject reward
     };
     const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior });
     const input = { input: "who should we hea", cursorPosition: 15 };
-    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD).map((wPred) => wPred.prediction);
+    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD)
+        .map((wPred) => wPred.prediction);
     expect(results).toEqual([
         "who should we heart attack",
         "who should we hepatitis",
+        "who should we heal",
+        "who should we health risk",
+    ]);
+});
+
+test("Predictions are returned even when there is no word to complete.", () => {
+    const languageSpecs = {
+        maxEditDistance: 1,
+        moduleType: LANGUAGE_MODULE_TYPE.FUZZY_TRIE_SEARCH,
+        tokenizerType: TOKENIZER_TYPE.WORD,
+    };
+    const rewardSpecs: RewardModuleSpecs = {
+        moduleType: REWARD_MODULE_TYPE.PROB_OF_NOT_REJECTING_SYMBOLS_GAINED,
+        rejectionLogit: 0,
+    };
+    const triePipeline = initializeLTVWithContext(languageSpecs, rewardSpecs, { trie: testTree, prior });
+    const input = { input: "who should we ", cursorPosition: 14 };
+    const results = triePipeline.predict(input, 5, 0, QUALITY_MODULE_TYPE.EXPECTED_REWARD)
+        .map((wPred) => wPred.prediction);
+    expect(results).toEqual([
+        "who should we jaundice",
+        "who should we hepatitis",
+        "who should we heart attack",
         "who should we heal",
         "who should we health risk",
     ]);
